84a6c4a2d5d79e578469cef0ff573ac40e7582f6
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-09-28 20:10:50 -0300

    up

---
+++ Aula4/pratica2.py
@@ -0,0 +1,14 @@
+# Um furto de celular aconteceu na cidade do RJ e você foi contratado para desenvolver um programa que possa ajudar sua professora 
+# a identificar uma das pessoas envolvidas.
+# Desenvolva uma solução utilizando vetor que faça 5 perguntas para apenas uma pessoa, sendo elas:
+# Conhece a vítima do furto?
+# Esteve no local do furto?
+# Mora perto da vítima?
+# A vítima lhe devia?
+# Já trabalhou com a vítima?
+
+# Se a pessoa responder positivamente a 2 questões ela deve ser classificada como "Suspeita",
+# Entre 3 e 4 como "Cúmplice" e 5 como "Ladrão".
+# Caso contrário, ela será classificada como "Inocente".
+
+ 
