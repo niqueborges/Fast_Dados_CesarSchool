64099ded75d6ca26298d2f4727f0b80031f200a4
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-09-28 12:56:28 -0300

    up

---
+++ Aula2/pratica5.py
@@ -0,0 +1,3 @@
+# Ao tentar colocar um Voltorb em uma piscina, Biu acabou levando um choque do trovão. 
+# Curioso de saber qual foi a intensidade do choque que levou, ele pesquisou e descobriu que existia uma relação entre o level do voltorb e a potência de seu choque.
+# Escreva um programa que, dado o level do voltorb, imprima de quanto foi o choque em W segundo a tabela:
