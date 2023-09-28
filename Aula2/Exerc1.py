b8c74ddcc58e3323a4ea3b5e1ecce836a6a2d798
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-09-28 14:26:53 -0300

    up

---
+++ Aula2/Exerc1.py
@@ -0,0 +1,2 @@
+# Faça um algoritmo que leia a primeira letra do estado civil de uma pessoa e mostre uma mensagem com a sua descrição (Solteiro, Casado, Viúvo, Divorciado). 
+# Mostre uma mensagem de erro, caso a opção seja inválida.
---
+++ Aula2/Exerc4.py
@@ -0,0 +1,4 @@
+# Faça um programa que ao usuário informar a idade do atleta ele exiba na tela qual a sua categoria, conforme a lista a seguir:
+# Juvenil – idade entre 16 e 18 anos; Infantil – idade entre 14 e 15 anos; Mirim – idade entre 12 e 13 anos;
+# Pré-Mirim – idade entre 10 e 11 anos.
+# Caso a idade não se enquadre em nenhuma categoria, exiba a mensagem: “Categoria não registrada”.
