d7007b9e90627b591ae142e4bff164717dccf5f3
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-09-29 13:22:20 -0300

    up

---
+++ Aula4/Exerc3.py
@@ -0,0 +1,4 @@
+# Leia a idade de 4 pessoas e armazene-as em uma lista. Após esta entrada de dados, exiba:
+# i) As idades armazenadas;
+# ii) Média das idades;
+# iii) Maior e menor idade (pode usar max e min);
