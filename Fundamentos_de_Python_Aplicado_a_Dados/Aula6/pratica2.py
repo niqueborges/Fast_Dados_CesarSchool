da52f06a15aeb87866fb5f2cab7752443425c69b
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-10-02 20:12:22 -0300

    up

--- Fundamentos_de_Python_Aplicado_a_Dados/Aula6/pratica2.py
+++ Fundamentos_de_Python_Aplicado_a_Dados/Aula6/pratica2.py
@@ -1,1 +1,7 @@
 """Faça uma função que retorne o reverso de um número inteiro pelo usuário. Por exemplo: 127 -> 721"""
+
+def reverso(num):
+    return str(num)[::-1]
+
+numero = int(input('Digite um número inteiro: '))
+print(reverso(numero))
---
+++ Fundamentos_de_Python_Aplicado_a_Dados/Aula6/pratica3.py
@@ -0,0 +1,9 @@
+"""Faça uma função que calcule a média de um aluno (duas notas) de acordo com o critério definido neste curso.
+Além, disso, faça uma segunda função que informe o status do aluno de acordo com a tabela a seguir:
+
+Nota acima de 6.0: Aprovado
+Nota entre 4.0 e 6.0: Verificaçao Suplementar
+Nota abaixo de 4.0: Reprovado
+
+Utilizar duas funções"""
+
