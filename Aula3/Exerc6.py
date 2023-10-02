674556e80849ce831b8e37f79dc9e0042a0cafba
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-10-02 13:11:22 -0300

    up

--- Aula3/Exerc6.py
+++ Aula3/Exerc6.py
@@ -12,6 +12,6 @@ while i * (i+1) * (i+2) < n:
     i = i + 1
 
 if i * (i+1) * (i+2) == n:
-    print('%d eh o produto %d*%d*%d' %(n,i,i+1,i+2))
+    print('%d é o produto %d*%d*%d' %(n,i,i+1,i+2))
 else:
-    print('%d nao eh triangular' %(n))
+    print('%d nao é triangular' %(n))
