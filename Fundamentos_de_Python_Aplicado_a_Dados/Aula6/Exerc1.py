b56c36ccd139c91e3b6185521d5d529051522bc5
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-10-02 21:30:31 -0300

    up

--- Fundamentos_de_Python_Aplicado_a_Dados/Aula6/Exerc1.py
+++ Fundamentos_de_Python_Aplicado_a_Dados/Aula6/Exerc1.py
@@ -3,3 +3,23 @@ A partir da escolha do usuário, o programa deverá chamar a função específic
 A função para converter horas em minutos deverá receber como parâmetro a hora e retornar o valor em minutos. 
 A função para converter horas em segundos deverá receber como parâmetro a hora e retornar o valor em segundos. 
 Por fim, o programa principal imprime o valor retornado pela função. """
+
+def converter_para_minutos(horas):
+    min = horas * 60
+    return min
+
+def converter_para_segundos(horas):
+    segundos = horas * 3600
+    return segundos
+
+horas = int(input('Informe o valor em horas: '))
+escolha = input('Digite [M] para converter em minutos ou [S] para converter em segundos: ').upper()
+
+if escolha.upper() == 'M':
+    converter_para_minutos(horas)
+    print(f'{horas} horas equivalem a {min} minutos.')
+elif escolha.upper() == 'S':
+    converter_para_segundos(horas)
+    print(f'{horas} horas equivalem a {segundos} segundos.')
+else:
+    print('Entrada inválida.')
