dea4f72c89c18697c2487e02e499c4f176c3bb3e
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-10-02 21:32:05 -0300

    up

--- Fundamentos_de_Python_Aplicado_a_Dados/Aula6/Exerc2.py
+++ Fundamentos_de_Python_Aplicado_a_Dados/Aula6/Exerc2.py
@@ -2,3 +2,15 @@
 (deve parar quando o valor digitado for igual a zero). 
 Como retorno da função, para cada valor lido deverá ser imprimido a sequência de 1 até X (o número digitado), 
 com um espaço entre cada número e seu sucessor. """
+
+def imprimir_sequencia():
+    while True:
+        x = int(input('Informe número inteiro (0 para sair): '))
+        
+        if x == 0:
+            break
+        
+        for i in range(1, x+1):
+            print(i, end='')
+        
+        print()
