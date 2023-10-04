a645b89c43c1d487f8e3ad62c9c692a36afec707
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2023-10-04 20:30:56 -0300

    up

--- Pandas/testeee.py
+++ Pandas/testeee.py
@@ -1,9 +1,13 @@
-estados=['Alagoas', 'AL', '3,4 milhões', 'Maceió'],
-['Bahia', 'BA', '15,4 milhões', 'Salvador'],
-['Ceará', 'CE', '9,4 milhões', 'Fortaleza'],
-['Maranhão', 'MA', '7,1 milhões', 'São luiz'],
-['Paraiba', 'PB', '4.0 milhões', 'João Pessoa'],
-['Pernambuco', 'PE', '3,4 milhões', 'Maceió'],
-['Piauí', 'PI', '3,3 milhões', 'Teresina'],
-['Rio Grande do Norte', 'RN', '3,5 milhões', 'Natal'],
-['Sergipe', 'SE', '2,4 milhões', 'Aracaju'],
+import pandas as pd
+
+estados=[['Alagoas', 'AL', '3,4 milhões', 'Maceió'],
+         ['Bahia', 'BA', '15,4 milhões', 'Salvador'],
+         ['Ceará', 'CE', '9,2 milhões', 'Fortaleza'],
+         ['Maranhão', 'MA', '7,1 milhões', 'São luiz'],
+         ['Paraiba', 'PB', '4.0 milhões', 'João Pessoa'],
+         ['Pernambuco', 'PE', '3,4 milhões', 'Recife'],
+         ['Piauí', 'PI', '3,3 milhões', 'Teresina'],
+         ['Rio Grande do Norte', 'RN', '3,5 milhões', 'Natal'],
+         ['Sergipe', 'SE', '2,4 milhões', 'Aracaju']]
+df = pd.DataFrame(estados,columns=['Estado', 'Sigla', 'População', 'Capital'])
+print(df)
